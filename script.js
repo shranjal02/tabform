b15d8a6fe5726e1d5c4ff64bbfef5235d5206a76
Shranjal Agrawal <ec1954@global.org.in>
2022-06-27 19:17:22 +0530

    Tab form

---
+++ script.js
@@ -0,0 +1,9 @@
+const BtnAdd = document.querySelector(".btn-add");
+const DivContainer = document.getElementById("div-container");
+BtnAdd.addEventListener("click", AddNew);
+function AddNew(){
+    const newDiv = document.createElement("div");
+    console.log("add");
+    newDiv.classList.add('div-shadow');
+    document.body.appendChild(newDiv);
+}
